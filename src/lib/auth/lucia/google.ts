e43d52626479287334f64bce95fd3a0b1834957b
Unies Ananda Raja <uanandaraja@gmail.com>
2024-09-26 22:09:48 +0700

    fix: use BASE_URL env variable for Google callback URL

--- src/lib/auth/lucia/google.ts
+++ src/lib/auth/lucia/google.ts
@@ -3,5 +3,5 @@ import { Google } from "arctic";
 export const google = new Google(
   process.env.GOOGLE_CLIENT_ID!,
   process.env.GOOGLE_CLIENT_SECRET!,
-  "http://localhost:3000/api/auth/google/callback",
+  `${process.env.BASE_URL}/api/auth/google/callback`,
 );
