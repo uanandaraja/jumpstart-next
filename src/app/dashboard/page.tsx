949d3518365f1851ef2854bb9c7c35094c25cd54
Unies Ananda Raja <uanandaraja@gmail.com>
2024-09-26 22:23:50 +0700

    chore: fix copy

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -24,7 +24,7 @@ export default function Dashboard() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ol className="list-inside list-decimal text-sm space-y-2 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li>This is Dashboard</li>
+          <li>This is a protected route</li>
           <li>If you aint logged in, you not supposed to see this.</li>
           <li>You are: {user?.name || "Unknown"}</li>
         </ol>
