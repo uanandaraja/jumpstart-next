afb1fee7539804ab431dbb6147756df006aadad5
Unies Ananda Raja <uanandaraja@gmail.com>
2024-09-26 21:55:34 +0700

    fix: place loading in center

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,17 @@ import { useUser } from "@/hooks/useUser";
 export default function Dashboard() {
   const { data: user, isLoading, error } = useUser();
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading)
+    return (
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center">
+          <div className="text-sm font-[family-name:var(--font-geist-mono)]">
+            Loading...
+          </div>
+        </main>
+      </div>
+    );
+
   if (error) return <div>Error: {error.message}</div>;
 
   return (
