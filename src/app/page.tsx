af17f18ad1735f1e4e514b3f319dbb9edda92b04
Unies Ananda Raja <uanandaraja@gmail.com>
2024-09-26 21:54:23 +0700

    chore: copywriting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start max-w-4xl mx-auto">
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
@@ -14,7 +14,10 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">Get started with your project quickly.</li>
-          <li>Use modern tools.</li>
+          <li className="max-w-lg">
+            Here we use: Tanstack React Query, Shadcn UI, Drizzle ORM with
+            Postgres, Lucia auth with Google OAuth
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -26,7 +29,7 @@ export default function Home() {
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://github.com/uanandaraja/jumpstart-next"
             target="_blank"
             rel="noopener noreferrer"
           >
